1dbf29d43a24811b0805d5b6540eba8d9b15eda0
Justin <jdclav@udel.edu>
2022-12-05 22:12:22 -0500

    Added input and functionality to editing

--- src/CreateScreen.tsx
+++ src/CreateScreen.tsx
@@ -6,15 +6,19 @@ type screenProps = {
     updatePage: (newPage: number) => void;
     changeXSize: (event: React.ChangeEvent<HTMLInputElement>) => void;
     changeYSize: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    xSize: number;
+    ySize: number;
 };
 
 const CreateScreen: React.FC<screenProps> = (props) => {
-    const { updatePage, changeXSize, changeYSize } = props;
+    const { updatePage, changeXSize, changeYSize, xSize, ySize } = props;
     return (
         <div>
             <GridEdit
                 changeXSize={changeXSize}
                 changeYSize={changeYSize}
+                xSize={xSize}
+                ySize={ySize}
             ></GridEdit>
             <Button
                 className="setup_page_button"
--- src/GridEdit.tsx
+++ src/GridEdit.tsx
@@ -4,19 +4,29 @@ import { Form } from "react-bootstrap";
 type ObjectProp = {
     changeXSize: (event: React.ChangeEvent<HTMLInputElement>) => void;
     changeYSize: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    xSize: number;
+    ySize: number;
 };
 
 const GridEdit: React.FC<ObjectProp> = (props) => {
-    const { changeXSize, changeYSize } = props;
+    const { changeXSize, changeYSize, xSize, ySize } = props;
 
     return (
         <div>
             <div>Change Grid size</div>
             <Form.Group controlId="formShortAnswer">
                 <Form.Label>X:</Form.Label>
-                <Form.Control type="number" onChange={changeXSize} />
+                <Form.Control
+                    type="number"
+                    value={xSize}
+                    onChange={changeXSize}
+                />
                 <Form.Label>Y:</Form.Label>
-                <Form.Control type="number" onChange={changeYSize} />
+                <Form.Control
+                    type="number"
+                    value={ySize}
+                    onChange={changeYSize}
+                />
             </Form.Group>
         </div>
     );
